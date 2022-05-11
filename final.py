aa22926c76036026ef88d7215e3b782b1e805bc1
Maliah <91290788+maliah-c@users.noreply.github.com>
2022-05-11 21:22:33 +0200

    Update final.py

--- final.py
+++ final.py
@@ -45,7 +45,7 @@ st.dataframe(df_new)
 
 #create table by country selection, using sort_value to order the table in descending order, using .agg() to split ecContribution into two columns (count, sum)
 con = sqlite3.connect("ecsel_database.db")
-df_best = pd.read_sql(f'SELECT country, organizations.shortname, organizations.name, organizations.activityType, organizations.organizationURL, organizations.ecContribution, country  FROM organizations WHERE country="{country}" ORDER BY ecContribution DESC', con)
+df_best = pd.read_sql(f'SELECT organizations.shortname, organizations.name, organizations.activityType, organizations.organizationURL, organizations.ecContribution, country  FROM organizations WHERE country="{country}" ORDER BY ecContribution DESC', con)
 
 
 df_better = pd.read_sql(f'SELECT organizations.shortname, organizations.name, organizations.activityType, organizations.projectAcronym, country FROM organizations WHERE country="{country}"', con)
